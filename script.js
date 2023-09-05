b597eaaf7a9f3ce77c09cc2c032c6b0cc49f4ebb
KallaiT <119456437+KallaiT@users.noreply.github.com>
2023-09-05 09:18:20 +0200

    dsadsadsadasd

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const h1element = document.querySelector('h1')
 h1element.addEventListener('click', function katt() {'alert'})
+
+const h2element = document.querySelector('h2')
+h2element.addEventListener('click', function katt() {'alert'})
